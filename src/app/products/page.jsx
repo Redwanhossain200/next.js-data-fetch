9c3d91b29b47d26320a5d368fa5883818fd134c3
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-04-24 14:27:50 +0600

    update file

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,7 +1,9 @@
 import Product from '@/components/Product';
 
 const getProducts = async () => {
-  const res = await fetch('http://localhost:5000/products');
+  const res = await fetch('http://localhost:5000/products', {
+    cache: 'force-cache',
+  });
   return res.json();
 };
 
@@ -10,7 +12,7 @@ const page = async () => {
   return (
     <div className="container mx-auto p-6">
       <h2 className="text-center py-4 text-4xl text-fuchsia-300 font-semibold">
-        Products : {products.length}{' '}
+        Products : {products.length}
       </h2>
       <div className="grid grid-cols-3 gap-4">
         {products.map((product) => (
